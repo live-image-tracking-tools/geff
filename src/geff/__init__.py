92292837715a3c9d2ca42da84d10e236c0c3fac5
Jan Funke <funkej@janelia.hhmi.org>
2025-07-18 14:15:36 -0400

    Add spatial_graph as optional dependency

--- src/geff/__init__.py
+++ src/geff/__init__.py
@@ -9,4 +9,23 @@ from .metadata_schema import GeffMetadata
 from .networkx.io import read_nx, write_nx
 from .utils import validate
 
-__all__ = ["GeffMetadata", "read_nx", "validate", "write_nx"]
+
+def __getattr__(name: str):
+    if name == "read_sg":
+        try:
+            from .spatial_graph.io import read_sg
+
+            return read_sg
+        except ImportError:
+            raise ImportError("install with geff[spatial_graph] to use read_sg")
+    if name == "write_sg":
+        try:
+            from .spatial_graph.io import write_sg
+
+            return write_sg
+        except ImportError:
+            raise ImportError("install with geff[spatial_graph] to use read_sg")
+    raise AttributeError(f"module '{__name__}' has no attribute '{name}'")
+
+
+__all__ = ["GeffMetadata", "read_nx", "read_sg", "validate", "write_nx", "write_sg"]
