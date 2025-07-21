105703ae53cdc411eea7dd3de3e6c9094ea85a8b
melisande-c <melisande.croft@fht.org>
2025-07-21 17:52:13 -0400

    docs: add docstrings to unified read func

--- src/geff/io/read.py
+++ src/geff/io/read.py
@@ -1,54 +1,91 @@
 from pathlib import Path
-from typing import (
-    TYPE_CHECKING,
-    Any,
-    Literal,
-    Protocol,
-    TypeVar,
-    overload,
-)
+from typing import Any, Literal, Protocol, TypeVar, overload
 
 import networkx as nx
 
-from ..dict_representation import GraphDict
-from ..geff_reader import read_to_dict
-from ..metadata_schema import Axis, GeffMetadata
-from ..networkx.io import _ingest_dict_nx
-from .supported_backends import SupportedBackend
+from geff.dict_representation import GraphDict
+from geff.geff_reader import read_to_dict
+from geff.metadata_schema import GeffMetadata
+from geff.networkx.io import _ingest_dict_nx
 
-if TYPE_CHECKING:
-    import networkx as nx
+from .supported_backends import SupportedBackend
 
 R = TypeVar("R", covariant=True)
 
 
 class IngestFunc(Protocol[R]):
-    def __call__(self, graph_dict: GraphDict, *args, **kwargs) -> tuple[R, GeffMetadata]: ...
+    """A protocol for callables that ingest a `GraphDict` to different backends."""
+
+    def __call__(self, graph_dict: GraphDict, *args: Any, **kwargs: Any) -> tuple[R, GeffMetadata]:
+        """
+        The callable must have this function signature.
+
+        The callable must have the first argument `graph_dict`, it may have additional
+        args and kwargs.
+
+        Args:
+            graph_dict (GraphDict): A graph representation of the GEFF data.
+            *args (Any): Optional args for ingesting the `graph_dict`.
+            **kwargs (Any): Optional kwargs for ingesting the `graph_dict`.
+        """
+        ...
 
 
-def ingest_dict_dummy(graph_dict: GraphDict) -> tuple[int, GeffMetadata]:
-    return 1, GeffMetadata(geff_version="0.3.0", directed=True, axes=[Axis(name="x")])
+# !!! When a new backend is added new overloads for `read` should be added !!!
 
 
-@overload
-def get_ingest_func(
-    backend: Literal[SupportedBackend.NETWORKX],
-) -> IngestFunc[nx.Graph | nx.DiGraph]: ...
+# When the GRAPH_DICT option is removed from SupportedBackend enum this can be removed.
+# Currently need 2 options for the overloads to work properly
+def ingest_graph_dict(graph_dict: GraphDict) -> tuple[GraphDict, GeffMetadata]:
+    """
+    This functional is essentially the identity.
 
-
-@overload
-def get_ingest_func(backend: Literal[SupportedBackend.DUMMY]) -> IngestFunc[int]: ...
+    Args:
+        graph_dict (GraphDict): A graph representation of the GEFF data.
+    """
+    return graph_dict, graph_dict["metadata"]
 
 
 def get_ingest_func(backend: SupportedBackend) -> IngestFunc[Any]:
+    """
+    Get the ingest function for different backends.
+
+    Args:
+        backend (SupportedBackend): Flag for the chosen backend.
+
+    Returns:
+        IngestFunc: A function to ingest a `GraphDict` to the chosen backend.
+    """
     match backend:
         case SupportedBackend.NETWORKX:
-            ingest_func = _ingest_dict_nx
-        case SupportedBackend.DUMMY:
-            ingest_func = ingest_dict_dummy
+            return _ingest_dict_nx
+        case SupportedBackend.GRAPH_DICT:
+            return ingest_graph_dict
+        # Add cases for new backends, remember to add overloads
         case _:
-            raise ValueError
-    return ingest_func
+            raise ValueError(f"Unsupported backend chosen: '{backend.value}'")
+
+
+@overload
+def read(
+    path: Path | str,
+    validate: bool = True,
+    node_props: list[str] | None = None,
+    edge_props: list[str] | None = None,
+    backend: Literal[SupportedBackend.NETWORKX] = SupportedBackend.NETWORKX,
+    backend_kwargs: dict[str, Any] | None = None,
+) -> tuple[nx.Graph | nx.DiGraph, GeffMetadata]: ...
+
+
+@overload
+def read(
+    path: Path | str,
+    validate: bool,
+    node_props: list[str] | None,
+    edge_props: list[str] | None,
+    backend: Literal[SupportedBackend.GRAPH_DICT],
+    backend_kwargs: dict[str, Any] | None = None,
+) -> tuple[GraphDict, GeffMetadata]: ...
 
 
 def read(
@@ -56,9 +93,31 @@ def read(
     validate: bool = True,
     node_props: list[str] | None = None,
     edge_props: list[str] | None = None,
-    ingest_func: IngestFunc[R] = _ingest_dict_nx,
+    backend: SupportedBackend = SupportedBackend.NETWORKX,
     backend_kwargs: dict[str, Any] | None = None,
-) -> tuple[R, GeffMetadata]:
+) -> tuple[Any, GeffMetadata]:
+    """
+    Read a GEFF to a chosen backend.
+
+    Args:
+        path (Path | str): The path to the root of the geff zarr, where the .attrs contains
+            the geff  metadata
+        validate (bool, optional): Flag indicating whether to perform validation on the
+            geff file before loading into memory. If set to False and there are
+            format issues, will likely fail with a cryptic error. Defaults to True.
+        node_props (list of str, optional): The names of the node properties to load,
+            if None all properties will be loaded, defaults to None.
+        edge_props (list of str, optional): The names of the edge properties to load,
+            if None all properties will be loaded, defaults to None.
+        backend (SupportedBackend): Flag for the chosen backend, default is "networkx".
+        backend_kwargs (dict of {str: Any}): Additional kwargs that may be accepted by
+            the backend when reading the data.
+
+    Returns:
+        graph (Any): Graph object of the chosen backend.
+        metadata (GeffMetadata): The GEFF metadata.
+    """
+    ingest_func = get_ingest_func(backend)
     if backend_kwargs is None:
         backend_kwargs = {}
     graph_dict = read_to_dict(path, validate, node_props, edge_props)
