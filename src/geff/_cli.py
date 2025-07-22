507753124011e4cc3b8dd112b07d0b52f5db01bd
Yohsuke Fukai <ysk@yfukai.net>
2025-07-22 13:55:28 -0400

    update

--- src/geff/_cli.py
+++ src/geff/_cli.py
@@ -1,16 +1,12 @@
 import argparse
 
-from .utils import validate
+import utils
 
 
-def main() -> None:
+def validate() -> None:
     """Command-line interface for validating geff files."""
     parser = argparse.ArgumentParser(description="Validate a GEFF file")
     parser.add_argument("input_path", help="Path to the GEFF file")
     args = parser.parse_args()
-    validate(args.input_path)
+    utils.validate(args.input_path)
     print(f"{args.input_path} is valid")
-
-
-if __name__ == "__main__":
-    main()
