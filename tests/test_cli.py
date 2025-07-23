875ec72f3ccdc71cd2644265a616be81513bc6b9
Yohsuke Fukai <ysk@yfukai.net>
2025-07-23 11:30:50 -0400

    updated test using geff.testing.data

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,8 +3,20 @@ import sys
 import pytest
 import zarr
 
+import geff
 import geff._cli as cli
 from geff.metadata_schema import GeffMetadata
+from geff.testing.data import create_simple_temporal_geff
+
+
+@pytest.fixture
+def example_geff_path(tmp_path):
+    file_path = str(tmp_path / "test.geff")
+    store, graph_props = create_simple_temporal_geff()
+    # zarr.group(store).copy_store(zarr.open(file_path, mode="w"))
+    graph, metadata = geff.read_nx(store)
+    geff.write_nx(graph, file_path, metadata=metadata)
+    return file_path
 
 
 def run_cli_with_args(args, monkeypatch):
@@ -13,23 +25,17 @@ def run_cli_with_args(args, monkeypatch):
     cli.main()
 
 
-def test_validate_command_prints_valid(capsys, monkeypatch, path_w_expected_graph_props):
+def test_validate_command_prints_valid(capsys, monkeypatch, example_geff_path):
     """Test that the validate command prints the expected output."""
-    path, graph_props = path_w_expected_graph_props(
-        "int8", {"position": "double"}, {"score": "float64", "color": "uint8"}, True
-    )
-    run_cli_with_args(["validate", str(path)], monkeypatch)
+    run_cli_with_args(["validate", example_geff_path], monkeypatch)
     captured = capsys.readouterr()
-    assert captured.out == f"{path} is valid\n"
+    assert captured.out == f"{example_geff_path} is valid\n"
 
 
-def test_info_command_prints_metadata(capsys, monkeypatch, path_w_expected_graph_props):
-    path, graph_props = path_w_expected_graph_props(
-        "int8", {"position": "double"}, {"score": "float64", "color": "uint8"}, True
-    )
-    run_cli_with_args(["info", str(path)], monkeypatch)
+def test_info_command_prints_metadata(capsys, monkeypatch, example_geff_path):
+    run_cli_with_args(["info", example_geff_path], monkeypatch)
     captured = capsys.readouterr()
-    metadata = GeffMetadata.read(zarr.open(path, mode="r"))
+    metadata = GeffMetadata.read(zarr.open(example_geff_path, mode="r"))
     assert captured.out == metadata.model_dump_json(indent=2) + "\n"
 
 
