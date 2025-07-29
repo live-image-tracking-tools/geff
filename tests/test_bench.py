fe7275d40648bb20c587862cc55f95758b9d2dba
Morgan Schwartz <msschwartz21@gmail.com>
2025-07-29 12:13:17 -0400

    Add rustworkx write test and also test spatial graph in benchmarking

--- tests/test_bench.py
+++ tests/test_bench.py
@@ -19,9 +19,11 @@ from typing import TYPE_CHECKING, Any
 import networkx as nx
 import numpy as np
 import rustworkx as rx
+import spatial_graph as sg
 
 import geff.networkx.io as geff_nx
 import geff.rustworkx.io as geff_rx
+import geff.spatial_graph.io as geff_sg
 from geff.utils import validate
 
 if TYPE_CHECKING:
@@ -75,8 +77,42 @@ def create_nx_graph(num_nodes: int) -> nx.DiGraph:
 def create_rx_graph(num_nodes: int) -> rx.PyDiGraph:
     graph = rx.PyDiGraph()
     nodes, edges = node_data(num_nodes), edge_data(num_nodes)
-    graph.add_nodes_from(nodes.items())
+    graph.add_nodes_from(nodes.values())
     graph.add_edges_from(((u, v, dd) for (u, v), dd in edges.items()))
+    return graph
+
+
+def create_sg_graph(num_nodes: int) -> sg.SpatialDiGraph:
+    nodes, edges = node_data(num_nodes), edge_data(num_nodes)
+    # Construct arrays of properties
+    node_ids, positions = [], []
+    for nid, attrs in nodes.items():
+        positions.append([attrs["t"], attrs["z"], attrs["y"], attrs["x"]])
+        node_ids.append(nid)
+    node_ids = np.array(node_ids)
+    positions = np.array(positions)
+
+    edge_ids, float_prop, int_prop = [], [], []
+    for eid, attrs in edges.items():
+        edge_ids.append(eid)
+        float_prop.append(attrs["float_prop"])
+        int_prop.append(attrs["int_prop"])
+    edge_ids = np.array(edge_ids)
+    float_prop = np.array(float_prop)
+    int_prop = np.array(int_prop)
+
+    create_graph = getattr(sg, "create_graph", sg.SpatialGraph)
+    graph = create_graph(
+        ndims=4,
+        node_dtype="int64",
+        node_attr_dtypes={"position": "float64[4]"},
+        edge_attr_dtypes={"float_prop": "float64", "int_prop": "int64"},
+        position_attr="position",
+        directed=True,
+    )
+    graph.add_nodes(node_ids, position=positions)
+    graph.add_edges(edge_ids, float_prop=float_prop, int_prop=int_prop)
+
     return graph
 
 
@@ -93,13 +129,14 @@ def graph_file_path(num_nodes: int) -> Path:
 CREATE_FUNCS: Mapping[Callable, Callable[[int], Any]] = {
     geff_nx.write_nx: create_nx_graph,
     geff_rx.write_rx: create_rx_graph,
+    geff_sg.write_sg: create_sg_graph,
 }
 
 # ###########################   TESTS   ##################################
 
 
 @pytest.mark.parametrize("nodes", [500])
-@pytest.mark.parametrize("write_func", [geff_nx.write_nx])
+@pytest.mark.parametrize("write_func", [geff_nx.write_nx, geff_rx.write_rx, geff_sg.write_sg])
 def test_bench_write(
     write_func: Callable, benchmark: BenchmarkFixture, tmp_path: Path, nodes: int
 ) -> None:
@@ -119,7 +156,7 @@ def test_bench_validate(benchmark: BenchmarkFixture, nodes: int) -> None:
 
 
 @pytest.mark.parametrize("nodes", [500])
-@pytest.mark.parametrize("read_func", [geff_nx.read_nx, geff_rx.read_rx])
+@pytest.mark.parametrize("read_func", [geff_nx.read_nx, geff_rx.read_rx, geff_sg.read_sg])
 def test_bench_read(read_func: Callable, benchmark: BenchmarkFixture, nodes: int) -> None:
     big_graph_path = graph_file_path(nodes)
     benchmark(read_func, big_graph_path, validate=False)
