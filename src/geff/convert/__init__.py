b03a932366b15761d888384efb1a74ad7a5136ee
Morgan Schwartz <msschwartz21@gmail.com>
2025-08-11 13:57:09 -0400

    interops -> convert

--- src/geff/convert/__init__.py
+++ src/geff/convert/__init__.py
@@ -1,6 +1,6 @@
 from typing import TYPE_CHECKING, Any
 
-from geff.interops.trackmate_xml import from_trackmate_xml_to_geff
+from geff.convert.trackmate_xml import from_trackmate_xml_to_geff
 
 if TYPE_CHECKING:
     from .ctc import ctc_tiffs_to_zarr, from_ctc_to_geff
@@ -10,10 +10,10 @@ __all__ = ["ctc_tiffs_to_zarr", "from_ctc_to_geff", "from_trackmate_xml_to_geff"
 
 def __getattr__(name: str) -> Any:
     if name == "ctc_tiffs_to_zarr":
-        from geff.interops.ctc import ctc_tiffs_to_zarr
+        from geff.convert.ctc import ctc_tiffs_to_zarr
 
         return ctc_tiffs_to_zarr
     if name == "from_ctc_to_geff":
-        from geff.interops.ctc import from_ctc_to_geff
+        from geff.convert.ctc import from_ctc_to_geff
 
         return from_ctc_to_geff
