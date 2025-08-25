cd78837dff22c7a8bae5ad4b7e14d6f2cbb28d1b
Talley Lambert <talley.lambert@gmail.com>
2025-08-25 10:57:22 -0400

    wip

---
+++ packages/geff-spec/src/geff_spec/__init__.py
@@ -0,0 +1,5 @@
+from ._model._prop_metadata import PropMetadata
+from ._model._schema import Axis, DisplayHint, GeffMetadata, RelatedObject
+
+__all__ = ["Axis", "DisplayHint", "GeffMetadata", "PropMetadata", "RelatedObject"]
+__version__ = "0.0.0"
--- packages/geff-spec/src/geff_spec/metadata/__init__.py
+++
@@ -1,4 +0,0 @@
-from ._prop_metadata import PropMetadata
-from ._schema import Axis, DisplayHint, GeffMetadata, RelatedObject
-
-__all__ = ["Axis", "DisplayHint", "GeffMetadata", "PropMetadata", "RelatedObject"]
