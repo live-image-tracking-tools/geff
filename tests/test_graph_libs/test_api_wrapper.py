50fe0e691f6a0bcec97fbd24eeb4a5a37949c5a0
melisande-c <melisande.croft@fht.org>
2025-09-04 11:12:25 +0200

    test: add construct test; refac test_api_wrapper to use new func in all tests

--- tests/test_graph_libs/test_api_wrapper.py
+++ tests/test_graph_libs/test_api_wrapper.py
@@ -5,11 +5,15 @@ import pytest
 
 from geff import read, write
 from geff._graph_libs._api_wrapper import SupportedBackend, get_backend
+from geff._graph_libs._backend_protocol import GraphAdapter
 from geff._graph_libs._networkx import NxBackend
+from geff._typing import InMemoryGeff
+from geff.core_io import read_to_memory
 from geff.testing.data import create_mock_geff
 
 if TYPE_CHECKING:
     from geff._graph_libs._backend_protocol import Backend
+
 
 rx = pytest.importorskip("rustworkx")
 sg = pytest.importorskip("spatial_graph")
@@ -25,35 +29,14 @@ extra_edge_props = [
 ]
 
 
-@pytest.mark.parametrize("node_id_dtype", node_id_dtypes)
-@pytest.mark.parametrize("node_axis_dtypes", node_axis_dtypes)
-@pytest.mark.parametrize("extra_edge_props", extra_edge_props)
-@pytest.mark.parametrize("directed", [True, False])
-@pytest.mark.parametrize("include_t", [True, False])
-@pytest.mark.parametrize("include_z", [True, False])
-@pytest.mark.parametrize("backend", get_args(SupportedBackend))
-def test_read(
-    node_id_dtype,
-    node_axis_dtypes,
-    extra_edge_props,
-    directed,
-    include_t,
-    include_z,
-    backend,
-) -> None:
-    backend_module: Backend = get_backend(backend)
-
-    store, memory_geff = create_mock_geff(
-        node_id_dtype,
-        node_axis_dtypes,
-        extra_edge_props=extra_edge_props,
-        directed=directed,
-        include_t=include_t,
-        include_z=include_z,
-    )
-
-    graph, metadata = read(store, backend=backend)
-    graph_adapter = backend_module.graph_adapter(graph)
+# assert that all the data in the graph are equal to those in the memory geff it was created from
+def _assert_graph_equal_to_geff(
+    graph_adapter: GraphAdapter,
+    memory_geff: InMemoryGeff,
+    include_t: bool,
+    include_z: bool,
+):
+    metadata = memory_geff["metadata"]
 
     # nodes and edges correct
     assert {*graph_adapter.get_node_ids()} == {*memory_geff["node_ids"].tolist()}
@@ -87,6 +70,73 @@ def test_read(
             graph_adapter.get_edge_prop(name, memory_geff["edge_ids"].tolist(), metadata),
             values,
         )
+
+
+@pytest.mark.parametrize("node_id_dtype", node_id_dtypes)
+@pytest.mark.parametrize("node_axis_dtypes", node_axis_dtypes)
+@pytest.mark.parametrize("extra_edge_props", extra_edge_props)
+@pytest.mark.parametrize("directed", [True, False])
+@pytest.mark.parametrize("include_t", [True, False])
+@pytest.mark.parametrize("include_z", [True, False])
+@pytest.mark.parametrize("backend", get_args(SupportedBackend))
+def test_read(
+    node_id_dtype,
+    node_axis_dtypes,
+    extra_edge_props,
+    directed,
+    include_t,
+    include_z,
+    backend,
+) -> None:
+    backend_module: Backend = get_backend(backend)
+
+    store, memory_geff = create_mock_geff(
+        node_id_dtype,
+        node_axis_dtypes,
+        extra_edge_props=extra_edge_props,
+        directed=directed,
+        include_t=include_t,
+        include_z=include_z,
+    )
+
+    graph, metadata = read(store, backend=backend)
+    graph_adapter = backend_module.graph_adapter(graph)
+
+    _assert_graph_equal_to_geff(graph_adapter, memory_geff, include_t, include_z)
+
+
+@pytest.mark.parametrize("node_id_dtype", node_id_dtypes)
+@pytest.mark.parametrize("node_axis_dtypes", node_axis_dtypes)
+@pytest.mark.parametrize("extra_edge_props", extra_edge_props)
+@pytest.mark.parametrize("directed", [True, False])
+@pytest.mark.parametrize("include_t", [True, False])
+@pytest.mark.parametrize("include_z", [True, False])
+@pytest.mark.parametrize("backend", get_args(SupportedBackend))
+def test_construct(
+    node_id_dtype,
+    node_axis_dtypes,
+    extra_edge_props,
+    directed,
+    include_t,
+    include_z,
+    backend,
+) -> None:
+    backend_module: Backend = get_backend(backend)
+
+    store, memory_geff = create_mock_geff(
+        node_id_dtype,
+        node_axis_dtypes,
+        extra_edge_props=extra_edge_props,
+        directed=directed,
+        include_t=include_t,
+        include_z=include_z,
+    )
+
+    in_memory_geff = read_to_memory(store)
+    graph = backend_module.construct(**in_memory_geff)
+    graph_adapter = backend_module.graph_adapter(graph)
+
+    _assert_graph_equal_to_geff(graph_adapter, memory_geff, include_t, include_z)
 
 
 @pytest.mark.parametrize("node_id_dtype", node_id_dtypes)
@@ -128,37 +178,6 @@ def test_write(
     graph, metadata = NxBackend.read(path_store)
     graph_adapter = NxBackend.graph_adapter(graph)
 
-    # nodes and edges correct
-    assert {*graph_adapter.get_node_ids()} == {*memory_geff["node_ids"].tolist()}
-    assert {*graph_adapter.get_edge_ids()} == {
-        *[tuple(edges) for edges in memory_geff["edge_ids"].tolist()]
-    }
-
-    # check node properties are correct
-    spatial_node_properties = ["y", "x"]
-    if include_t:
-        spatial_node_properties.append("t")
-    if include_z:
-        spatial_node_properties.append("z")
-    for name in spatial_node_properties:
-        np.testing.assert_array_equal(
-            graph_adapter.get_node_prop(name, memory_geff["node_ids"].tolist(), metadata=metadata),
-            memory_geff["node_props"][name]["values"],
-        )
-
-    for name, data in memory_geff["node_props"].items():
-        values = data["values"]
-        np.testing.assert_array_equal(
-            graph_adapter.get_node_prop(name, memory_geff["node_ids"].tolist(), metadata=metadata),
-            values,
-        )
-
-    # check edge properties are correct
-    for name, data in memory_geff["edge_props"].items():
-        values = data["values"]
-        np.testing.assert_array_equal(
-            graph_adapter.get_edge_prop(name, memory_geff["edge_ids"].tolist(), metadata),
-            values,
-        )
+    _assert_graph_equal_to_geff(graph_adapter, memory_geff, include_t, include_z)
 
     # TODO: test metadata
