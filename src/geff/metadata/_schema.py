03c7fa2fce2bc237a1f8cf5351ccbbb6a188ce58
Morgan Schwartz <msschwartz21@gmail.com>
2025-09-11 14:15:44 -0400

    Additional missing space

--- src/geff/metadata/_schema.py
+++ src/geff/metadata/_schema.py
@@ -342,7 +342,7 @@ class GeffMetadata(BaseModel):
         description=(
             "Node properties denoting tracklet and/or lineage IDs.\n"
             "A tracklet is defined as a simple path of connected nodes "
-            "where the initiating node has any incoming degree and outgoing degree at most 1,"
+            "where the initiating node has any incoming degree and outgoing degree at most 1, "
             "and the terminating node has incoming degree at most 1 and any outgoing degree, "
             "and other nodes along the path have in/out degree of 1. Each tracklet must contain "
             "the maximal set of connected nodes that match this definition - no sub-tracklets.\n"
